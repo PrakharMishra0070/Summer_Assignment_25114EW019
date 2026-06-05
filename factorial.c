19489cea80b00c100c80b67eb6ab533411e566a9
PrakharMishra0070 <prakharmishra4075@gmail.com>
2026-06-05 15:51:26 +0530

    Commit3

---
+++ factorial.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int i,number
+    long long factorial = 1;
+
+    printf("Enter a number : ");
+    scanf("%d",n);
+
+    for( i = 1; i<=n; i++);
+    factorial = factorial * i;
+
+    printf("The factorial of %d = %d",n,factorial);
+
+    return 0;
+}
