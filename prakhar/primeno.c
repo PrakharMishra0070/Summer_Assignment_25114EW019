1e86fd7f57bf55bf235cb61c3494ded46afb3dcb
PrakharMishra0070 <prakharmishra4075@gmail.com>
2026-06-05 17:26:01 +0530

    Commit9

---
+++ prakhar/primeno.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int num, i;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num <= 1) {
+        printf("Not a Prime Number");
+        return 0;
+    }
+
+    for (i = 2; i < num; i++) {
+        if (num % i == 0) {
+            printf("Not a Prime Number");
+            return 0;
+        }
+    }
+
+    printf("Prime Number");
+
+    return 0;
+}
