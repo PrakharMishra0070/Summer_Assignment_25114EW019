af466ad1b27a6920a4223175de3bcc3a96918bd9
PrakharMishra0070 <prakharmishra4075@gmail.com>
2026-06-17 18:00:26 +0530

    Commit53

---
+++ prakhar/linear_search.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[100], n, key, i;
+
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    scanf("%d", &key);
+
+    for(i = 0; i < n; i++)
+    {
+        if(a[i] == key)
+        {
+            printf("It is Found at position %d", i + 1);
+            return 0;
+        }
+    }
+
+    printf("Unable to Found");
+
+    return 0;
+}
