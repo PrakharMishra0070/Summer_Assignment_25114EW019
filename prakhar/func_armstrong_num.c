716589790c9cdf7cefad946ebc0aa29dcdb13149
PrakharMishra0070 <prakharmishra4075@gmail.com>
2026-06-17 13:38:35 +0530

    Commit46

---
+++ prakhar/func_armstrong_num.c
@@ -0,0 +1,25 @@
+#include <stdio.h> 
+int armstrong ( int  n )
+{
+    int sum =0 , reminder , temp = n ;
+
+    while ( temp != 0 )
+    {
+        remimder = temp % 10 ;
+        sum += remimder * remimder * remimder ;
+        temp = temp /10 ;
+    }
+    return sum == n ;
+}
+ int main ()
+ {
+    int n ;
+    scanf ( "%d ", &n ) ;
+
+    if (armstrong(n))
+    printf ( "Armstrong ") ;
+    else 
+    printf ("Not Armstrong") ;
+
+    return 0 ;
+ }
